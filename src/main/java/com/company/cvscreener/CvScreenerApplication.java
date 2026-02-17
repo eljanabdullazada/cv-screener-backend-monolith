fffcbb61b8ab708063866945937e519f62d88ee0
eljan_abdullazada <a3371977@gmail.com>
2026-02-18 01:10:51 +0400

    CV Screener folder structure created

---
+++ src/main/java/com/company/cvscreener/CvScreenerApplication.java
@@ -0,0 +1,13 @@
+package com.company.cvscreener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvScreenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvScreenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/cvscreener/CvScreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cvscreener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvScreenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
